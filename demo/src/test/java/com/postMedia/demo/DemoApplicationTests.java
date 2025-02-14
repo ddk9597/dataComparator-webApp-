0ad6155c4a45562b2a8240ac715dbcce9398aea8
DESKTOP-QUASJ20\user-pc <027619@naver.com>
2025-02-14 17:10:07 +0900

    웹앱 이식 준비..

---
+++ demo/src/test/java/com/postMedia/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postMedia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
